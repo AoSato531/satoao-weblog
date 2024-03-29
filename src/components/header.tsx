c43a9b833c77aadece55976c313a30e3e425d671
Ao Sato <blue.sugar.318@gmail.com>
2024-03-30 02:58:02 +0900

    no message

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Header() {
+  return (
+    <main>
+      <div className="bg-orange w-screen h-32 flex items-center justify-center ">
+        <div className="text-5xl text-yellow-800">
+        <Link href="/">さとあおのつぶやきばしょ</Link>
+        </div>
+      </div>
+    </main>
+  );
+}
