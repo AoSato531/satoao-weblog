af98dc463a28526f25dcf266f49c7aee4b46c0f5
Ao Sato <blue.sugar.318@gmail.com>
2024-04-07 18:46:36 +0900

    いったんコミット

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Header from "@/components/Header";
+import Header from "../../components/Header";
 import Linkbox from "@/components/LinkBox";
 import BreadCrumb from "@/components/Breadcrumb";
 import { Inter } from "next/font/google";
