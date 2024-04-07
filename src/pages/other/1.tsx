ac2900150f425d0e318ef5e0f2bfaadfb24bf14e
Ao Sato <blue.sugar.318@gmail.com>
2024-04-07 18:32:59 +0900

    コミット

--- src/pages/other/1.tsx
+++ src/pages/other/1.tsx
@@ -54,7 +54,23 @@ export default function Home() {
             <br />
             <SectionBlock sectionText="趣味" />
             <SubSectionBlock sectionText="音楽" />
+            中1でL'Arc～en～Cielを知ったのをきっかけに音楽を聴き始める。<br />
+            中学では流行りの音楽も聴きつつ、90年代J-POPにハマる。<br />
+            ドラム歴は通算5年（15歳〜20歳まで）。高校の軽音楽部で活動し、卒業後は埼玉でバンド活動。<br />
+            大学以降は電子音楽にもハマり、20代半ばごろにDJをかじる。<br />
             <br />
+            好きなアーティスト：<br />
+            L'Arc～en～Ciel, moumoon, Cymbals, JUDY AND MARY, YUKI, LINDBERG, Scandal, BUMP OF CHICKEN, <br />
+            DIR EN GREY, LOVE PSYCHEDELICO, 小室哲哉, Squarepusher, slipknot, Dream Theater, 神保彰　...etc<br />
+            <br />
+            <SubSectionBlock sectionText="ゲーム" />
+            ポケモンと音楽ゲーム（beatmania IIDX, DrumMania, Dance Dance Revolution）とぷよぷよを好む。<br />
+            ガチ寄りだけどめちゃくちゃ上手いわけではない、けど結構上手いくらいの腕前。<br />
+            <br />
+            <SubSectionBlock sectionText="雑学" />
+            雑学を覚えるのが好き。とくに数学や日本語や漢字の雑学が好き。<br />
+            クイズをやるわけではないが、機会があればクイズをやってみたい。<br />
+            クイズ系のYouTubeを観るのはかなり好き。<br />
             <br />
             <SectionBlock sectionText="SNSなどリンク" />
             <SubSectionBlock sectionText="X（旧Twitter）" />
