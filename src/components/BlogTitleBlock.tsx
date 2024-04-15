72b34cbd300f2d341286a86742ebc5ac0c755f15
Ao Sato <blue.sugar.318@gmail.com>
2024-04-15 14:20:55 +0900

    日付文字がおかしいので修正

--- src/components/BlogTitleBlock.tsx
+++ src/components/BlogTitleBlock.tsx
@@ -9,7 +9,7 @@ export default function Component({ title, date }: Props) {
       <div className="lg:text-3xl text-base flex-col font-bold h-auto lg:p-8 p-4">
         {title}
       </div>
-      <div className="lg:text-xm text-xs flex-col h-auto text-center">
+      <div className="lg:text-base text-xs flex-col h-auto text-center">
         {date}
       </div>
     </div>
