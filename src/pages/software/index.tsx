16d423b100f4b6ddc0ad6e5722867a7dea63785d
Ao Sato <blue.sugar.318@gmail.com>
2024-04-15 12:19:35 +0900

    つくったものTOPだけコンポーネントの適用を忘れていたので修正

--- src/pages/software/index.tsx
+++ src/pages/software/index.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/Header";
 import Linkbox from "@/components/LinkBox";
 import BreadCrumb from "@/components/Breadcrumb";
+import ContentBlock from "@/components/ContentBlock";
+import CategoryBlock from "@/components/CategoryBlock";
 import BackPage from "@/components/BackPage";
 import { Inter } from "next/font/google";
 import Head from 'next/head';
@@ -17,17 +19,14 @@ export default function Page() {
       </Head>
       <Header />
       <BreadCrumb text1={categoty_title} url1="/other"/>
-      <div className="bg-orange-content w-screen h-full flex flex-col items-center justify-center ">
-        <div className="text-3xl flex-col h-auto p-8">
-          {categoty_title}
-        </div>
-        <div className="text-xl flex-col h-auto p-8 text-center">
-          遊びでつくったものを載せていきます。
-        </div>
+      <ContentBlock>
+        <CategoryBlock categoty_title={categoty_title}>
+          ゲームに関することをつづっています。
+        </CategoryBlock>
         <div className="grid grid-cols-1 lg:grid-cols-2">
         </div>
         <BackPage/>
-      </div>
+      </ContentBlock>
     </main>
   );
 }
