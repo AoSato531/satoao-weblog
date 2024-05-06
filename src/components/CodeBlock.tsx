1ba7e153c0ed09ad0a80b3ad433e82d5988a28c5
Ao Sato <blue.sugar.318@gmail.com>
2024-05-07 02:56:45 +0900

    コードブロックにスクロールを追加

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Component({ children }: Props) {
   return (
-    <pre className="bg-gray-100 whitespace-pre-wrap w-blog-box-text p-4 lg:text-base text:xs">
+    <pre className="bg-gray-100 w-blog-box-text p-4 lg:text-base text:xs overflow-x-auto">
       <code>
         {children}
       </code>
